410dd2d600715fd3388971fe0276ab8f0fbc0553
Nicolas <nicolasgp.ec@gmail.com>
2024-10-21 10:03:38 -0300

    Feat institutional pages

---
+++ app/institucional/sobre-a-shoestyle/page.tsx
@@ -0,0 +1,40 @@
+
+
+export default function Page() {
+    return (
+        <main style={{padding:'0 48px', width:'80%', marginBottom:'32px'}}>
+            <h2>
+                Projeto de E-commerce de Calçados - ShoeStyle
+            </h2>
+            <p>
+                O ShoeStyle é um projeto de e-commerce desenvolvido como parte do meu portfólio, com o objetivo de criar uma plataforma de vendas de calçados online que ofereça uma experiência de usuário fluida e intuitiva. Com foco em design responsivo e usabilidade, o ShoeStyle permite que os usuários explorem uma coleção variada de calçados, com filtros de estilo, tamanho e preferências.
+            </p>
+            <p>
+                Desenvolvido com tecnologias modernas, empregando Next.js e TypeScript, o projeto visa demonstrar minha capacidade de construir aplicações escaláveis, eficientes e boa performance. A API foi empregada diretamente no NextJS, gerenciando os dados buscados e fornecendo controle de estoque.
+            </p>
+            <p>
+                O sistema de pagamento não foi implementado pois trata-se de um modelo de desenvolvimento.
+            </p>
+            <h3 style={{margin:'12px 0'}}>
+                Destaques do projeto:
+            </h3>
+            <ul>
+                <li>
+                    Catálogo interativo com filtros dinâmicos para uma experiência de navegação otimizada.
+                </li>
+                <li>
+                    Sistema completo de carrinho de compras e finalização de pedidos.
+                </li>
+                <li>
+                    Design moderno, responsivo e adaptável para diferentes dispositivos.
+                </li>
+                <li>
+                    Integração com banco de dados PostgreSQL diretamente no NextJS para atualização de estoque em tempo real.
+                </li>
+            </ul>
+            <p>
+                O ShoeStyle faz parte do meu portfólio como um projeto pessoal, demonstrando minha habilidade em desenvolver soluções front-end completas , focadas tanto na experiência do usuário, na responsividade do sistema e na entrega.
+            </p>
+        </main>
+    )
+}
---
+++ app/institucional/termo-de-uso/page.tsx
@@ -0,0 +1,32 @@
+
+
+export default function Page() {
+    return (
+        <main style={{padding:'0 48px', width:'80%', marginBottom:'32px'}}>
+            <h2>
+                Termo de Uso e Responsabilidade - ShoeStyle
+            </h2>
+            <p>
+                Bem-vindo ao <strong>ShoeStyle</strong>. Ao acessar e utilizar este site, você concorda com os termos e condições descritos abaixo. Este termo tem como objetivo esclarecer o propósito do projeto e limitar a responsabilidade quanto às informações exibidas.
+            </p>
+            <p>
+                <b>1. Finalidade do Projeto</b> O <b>ShoeStyle</b> é um projeto de portfólio desenvolvido com o propósito de demonstrar habilidades técnicas em desenvolvimento web, desenvolvimento front-end, design responsivo e integrações com APIs. Ele não constitui uma loja online real, e o uso deste site é destinado exclusivamente para fins de demonstração.
+            </p>
+            <p>
+                <b>2. Produtos e Ofertas</b> Os produtos, preços, descrições e ofertas exibidos no ShoeStyle <b>são fictícios e servem apenas para fins de simulação</b>. <b>Nenhuma</b> informação apresentada neste site deve ser interpretada como uma oferta real de venda ou uma promessa de compra. O <b>ShoeStyle</b> não comercializa produtos e <b>não realiza transações financeiras</b> de nenhum tipo.
+            </p>
+            <p>
+                <b>3. Isenção de Responsabilidade</b> O desenvolvedor do ShoeStyle não se responsabiliza por quaisquer decisões tomadas com base nas informações contidas neste site. Este projeto é puramente demonstrativo, e todas as informações sobre produtos, estoques e ofertas não possuem validade comercial.
+            </p>
+            <p>
+                <b>4. Uso de Dados</b> Embora este site possa coletar informações básicas, como nome e e-mail, esses dados são utilizados exclusivamente para fins de teste e demonstração das funcionalidades do sistema. Nenhuma informação pessoal será compartilhada com terceiros ou utilizada para fins comerciais.
+            </p>
+            <p>
+                <b>5. Alterações no Termo</b> O desenvolvedor do <b>ShoeStyle</b> se reserva o direito de modificar este Termo de Uso e Responsabilidade a qualquer momento. Qualquer alteração será publicada neste mesmo documento, mantendo os usuários informados sobre as mudanças.
+            </p>
+            <p>
+                <b>6. Contato</b> Caso tenha dúvidas sobre este Termo de Uso e Responsabilidade, entre em contato por meio das informações fornecidas no site.
+            </p>
+        </main>
+    )
+}
