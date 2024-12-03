ef158aece0d7793e12bc023152b707f31b002df7
Nicolas <nicolasgp.ec@gmail.com>
2024-12-03 18:02:24 -0300

    fix cart fetch.

--- app/lib/cartLoad.ts
+++ app/lib/cartLoad.ts
@@ -1,8 +1,7 @@
 import { ICartItems } from "../context/userContext/userContext";
-import { app_root } from "./root";
 
 export default async function cartLoad(user_id:string | undefined) {
-    const cartItems:ICartItems = await fetch(`${app_root}/api/cart?user_id=${user_id}`,{
+    const cartItems:ICartItems = await fetch(`/api/cart?user_id=${user_id}`,{
         method:"GET",
         // headers: {
         //     "Content-Type": "application/json",
